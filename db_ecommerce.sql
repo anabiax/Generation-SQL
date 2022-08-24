5a6b91f3785175f5233fa87e287251327451d98e
Ana Santos <anabia200113@gmail.com>
2022-08-24 19:46:20 -0300

    primeiros ex

--- db_ecommerce.sql
+++ db_ecommerce.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE db_ecommerce;
-
 USE db_ecommerce;
 
 CREATE TABLE tb_ecommerce (
@@ -11,17 +10,17 @@ CREATE TABLE tb_ecommerce (
     preco decimal(5,2)
 );
 
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("morte súbita", "Lola Cosmetics", "produto capilar", 200, 80.10);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("meu cacho, minha vida", "Lola Cosmetics", "produto capilar",300, 50.00);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("be(m)dita ghee ", "Lola Cosmetics", "produto capilar", 400,40.00);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("pinga", "Lola Cosmetics", "produto capilar", 500, 19.99);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("argan oil", "Lola Cosmetics","produto capilar", 30.00);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("comigo ninguém pode", "Lola Cosmetics", "produto capilar", 600,40.50);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("cereal killer","Lola Cosmetics","produto capilar", 700, 50.99);
-INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("chá latte","Lola Cosmetics","produto capilar", 800, 42.60);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("morte súbita", "Lola Cosmetics", "produto capilar", 200, 800.10);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("meu cacho, minha vida", "Lola Cosmetics", "produto capilar",300, 500.00);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("be(m)dita ghee ", "Lola Cosmetics", "produto capilar", 400,400.00);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("pinga", "Lola Cosmetics", "produto capilar", 500, 199.99);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("argan oil", "Lola Cosmetics","produto capilar", 300, 300.00);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("comigo ninguém pode", "Lola Cosmetics", "produto capilar", 600,700.50);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("cereal killer","Lola Cosmetics","produto capilar", 700, 500.99);
+INSERT INTO tb_ecommerce(nome, marca, categoria, quantidade, preco)VALUES("chá latte","Lola Cosmetics","produto capilar", 800, 920.60);
 
 SELECT * FROM tb_ecommerce;
-SELECT * FROM tb_ecommerce WHERE valor > 500;
-SELECT * FROM tb_ecommerce WHERE valor < 500;
+SELECT * FROM tb_ecommerce WHERE preco > 500.00;
+SELECT * FROM tb_ecommerce WHERE preco < 500.00;
 
-UPDATE tb_ecommerce SET valor=40.00 WHERE id=3;
+UPDATE tb_ecommerce SET marca="Salon line" WHERE id=6;
--- db_rh.sql
+++ db_rh.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE db_rh;
-
 USE db_rh;
 
 CREATE TABLE tb_func(
 	id bigint auto_increment primary key,
     nome varchar(255),
     data_nascimento date,
-    setor varchar(255),
+    cargo varchar(255),
     nivel varchar(255),
     salario decimal(6,2)
 );
 
-INSERT INTO tb_func(nome,data_nascimento, setor,nivel,salario)VALUES("Fulana de Tal", "2000-01-01", "front-end", "sênior", 9000.00);
-INSERT INTO tb_func(nome, data_nascimento, setor,nivel,salario)VALUES("Ciclana de Tal", "2001-02-02", "back-end","pleno", 5000.50);
-INSERT INTO tb_func(nome, data_nascimento, setor, nivel,salario)VALUES("Beltrana de Tal", "2002-03-03", "Q.A.", "pleno" , 2000.70);
-INSERT INTO tb_func(nome, data_nascimento, setor,nivel,salario)VALUES("Coisinha", "2003-04-04", "full stack","júnior", 1800.30);
-INSERT INTO tb_func(nome, data_nascimento, setor,nivel,salario)VALUES("Docinho", "2004-05-05","front-end", "júnior", 1300.80);
+
+INSERT INTO tb_func(nome,data_nascimento,cargo,nivel,salario)VALUES("Fulaninha de Tal", "2000-01-05", "frontend", "senior", 9000.00);
+INSERT INTO tb_func(nome, data_nascimento,cargo,nivel,salario)VALUES("Ciclana de Tal", "2001-02-02", "back-end","pleno", 5000.50);
+INSERT INTO tb_func(nome, data_nascimento,cargo,nivel,salario)VALUES("Beltrana de Tal", "2002-03-03", "Q.A.", "pleno" , 2000.70);
+INSERT INTO tb_func(nome, data_nascimento,cargo,nivel,salario)VALUES("Coisinha", "2003-04-04", "full stack","júnior", 1800.30);
+INSERT INTO tb_func(nome, data_nascimento,cargo,nivel,salario)VALUES("Docinho", "2004-05-05","front-end", "júnior", 1300.80);
 
 SELECT * FROM tb_func;
-SELECT * FROM tb_func WHERE salario >= 2000;
-SELECT * FROM tb_func WHERE salario <= 2000;
+SELECT * FROM tb_func WHERE salario > 2000;
+SELECT * FROM tb_func WHERE salario < 2000;
 
-UPDATE tb_alunes SET salario = 2000 WHERE id=2;
+UPDATE tb_alunes SET salario = 4000 WHERE id=2;
